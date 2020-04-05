994935d8e25c7a913e480c4fa46de206855bca85
vscharf <6690596+vscharf@users.noreply.github.com>
2020-04-05 14:08:21 +0200

    Move the repetitive tasks to create a figure to a dedicated plot

--- plots/cumulative_data.py
+++ plots/cumulative_data.py
@@ -1,28 +1,22 @@
-import matplotlib
-matplotlib.use('MacOSX')
-import matplotlib.pyplot as plt
-
-from utils import lower_bound_float, next_color, reset_next_color
+from utils import lower_bound_float, next_color, reset_next_color, Plot
 
 def plot_normalized_cumulative_data(title, filename, normalization_fraction, *args):
     """ Plots cumulative cases normalized to the first @normalization_fraction cases of the smallest total case count. Plot is saved as an png image to @filename .
 
 Data to plot is passed in in *args. The format for each data set is a tuple (legend, data), where @legend is the title that should appear on the legend, and @data is a list of cases per day."""
-    reset_next_color()
 
     smallest_case_count = min([data[-1] for _, data in args])
     normalization_boundary = normalization_fraction * smallest_case_count
 
     print "Using normalization point %.0f cases" % normalization_boundary
     
-    # create canvas
-    fig = plt.figure()
-    canvas = fig.add_subplot(111)
+    # create plot
+    plot = Plot(title=title)
 
     # figure out the index where all greater indices account for 95% of the total cases (round up)
     start_index = int(min([lower_bound_float(data, 0.05*data[-1]) for _, data in args]) + 0.5)
 
-
+    reset_next_color()
     # plot data is a list of x-points, y-point, format
     for (legend, data) in args:
         normalization_offset = lower_bound_float(data, normalization_boundary)
@@ -33,13 +27,8 @@ Data to plot is passed in in *args. The format for each data set is a tuple (leg
         # format
         format = next_color()
 
-        
+        plot.add_curve(legend, x, y, format)
 
-        canvas.plot(x, y, format, label=legend)
-
-    canvas.set_title(title)
-    canvas.set_xlabel("Days since %d cases (%.0f%% of smallest dataset)" % (normalization_boundary, 100.*normalization_fraction))
-    canvas.legend()
-    fig.savefig(filename)
-    
+    plot.xlabel = "Days since %d cases (%.0f%% of smallest dataset)" % (normalization_boundary, 100.*normalization_fraction)
+    plot.save(filename)
     
--- plots/utils.py
+++ plots/utils.py
@@ -1,3 +1,59 @@
+import matplotlib
+matplotlib.use('MacOSX')
+import matplotlib.pyplot as plt
+
+class Plot(object):
+    def __init__(self, title = None, xlabel = None):
+        # create canvas
+        self._fig = plt.figure()
+        self._canvas = self._fig.add_subplot(111)
+        self._title = title if title is not None else ""
+        if not isinstance(self._title, str):
+            raise TypeError("plots.utils.Plot.Plot: title: a string is required not %s" % type(value))
+
+        self._xlabel = xlabel if xlabel is not None else ""
+        if not isinstance(self._xlabel, str):
+            raise TypeError("plots.utils.Plot.Plot: xlabel: a string is required not %s" % type(value))
+
+
+    @property
+    def title(self):
+        return self._title
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise TypeError("plots.utils.Plot.title: a string is required not %s" % type(value))
+        self._title = value
+
+
+    @property
+    def canvas(self):
+        return self._canvas
+
+
+    @property
+    def xlabel(self):
+        return self._xlabel
+
+    @xlabel.setter
+    def xlabel(self, value):
+        print "xlabel set"
+        if not isinstance(value, str):
+            raise TypeError("plots.utils.Plot.xlabel: a string is required not %s" % type(value))
+        self._xlabel = value
+
+
+    def add_curve(self, legend, x, y, format):
+        self._canvas.plot(x, y, format, label=legend)
+
+
+    def save(self, filename):
+        self._canvas.set_title(self._title)
+        self._canvas.set_xlabel(self._xlabel)
+        self._canvas.legend()
+        self._fig.savefig(filename)
+        
+
 def lower_bound_float(data, bound):
     index_lower_bound = 1
     while data[index_lower_bound] < bound:
